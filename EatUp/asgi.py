abd9b8a4e0e8fbd491f8296fdc814e474d61ab34
omoleen <Oreoluwa123>
2023-06-19 14:08:25 +0100

    asgi updated

--- EatUp/asgi.py
+++ EatUp/asgi.py
@@ -10,19 +10,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EatUp.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from .websockets_middleware import JWTAuthMiddleware
 from riderapp.routing import url_patterns as rider_urls
 from users.routing import url_patterns as user_urls
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EatUp.settings')
-
-# application = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         # Just HTTP for now. (We can add other protocols later.)
         "websocket": JWTAuthMiddleware(URLRouter(rider_urls + user_urls))
     }
